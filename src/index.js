b9c26c56a0adbd4c157d11e241937c6a6c12e64b
Casey Harding <casey.harding@generalassemb.ly>
2021-09-14 12:24:37 -0400

    First form functional.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 // 👉 App contains a more sophisticated form we'll flesh out later
 import App from './components/App'
@@ -10,16 +10,41 @@ const petsList = [
   { petName: 'Goldie', petType: 'fish' },
 ]
 
+const initialFormValues = { petName: "", petType: "" }
+
 function SimpleForm() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const change = (evt) => {
+    const { name, value } = evt.target;
+    // const name = evt.target.name;
+    // const value = evt.target.value;
+    setFormValues({ ...formValues, [name]: value });
+  }
+
   return (
     <div className="container">
       <h1>Simple Form App</h1>
-
       {petsList.map((pet, idx) => (
         <div key={idx}>
           {pet.petName} is a {pet.petType}
         </div>
       ))}
+      <form>
+        <input
+          type="text"
+          value={formValues.petType}
+          name="petType"
+          onChange={change}
+        />
+        <input
+          type="text"
+          value={formValues.petName}
+          name="petName"
+          onChange={change}
+        />
+        <input type="submit" value="submit" />
+      </form>
     </div>
   )
 }
